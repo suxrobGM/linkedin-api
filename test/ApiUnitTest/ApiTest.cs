3b82989aee1284c44210ae5434dbe6a062aec66c
SuxrobGM <suxrobGM@gmail.com>
2019-06-18 11:36:00 +0500

    added test method of GetOwnProfile

--- test/ApiUnitTest/ApiTest.cs
+++ test/ApiUnitTest/ApiTest.cs
@@ -1,14 +1,15 @@
 using System;
 using Xunit;
 using LinkedIn.Api;
+using LinkedIn.Api.People;
 
 namespace ApiUnitTest
 {
     public class ApiTest
     {
-        private readonly string clientId = "YOUR_CLIENT_ID";
-        private readonly string clientSecret = "YOUR_CLIENT_SECRET";
-        private readonly string token = "YOUR_ACCESS_TOKEN";
+        private readonly string clientId = "77cme2vqhhzgyz";
+        private readonly string clientSecret = "Nj4oDp7u8OC9FkJt";
+        private readonly string token = "AQUrSPHI-a43ZER82wbasxt3N3PZSFvD_a-fgnNdfEDApxlUGtPMi5e_gEa3TghyjYWCCcmTo0OEt_SOmhIzbo-DPpLKFCox5uKKFvinR61gta7BemMOW5YV5F63RYZLshPXCSYhQcd1DWWWVqBuVCeyrvFDPj-WZVj3C-Nc7AEXyYfeAciR0H0-vgnqEjOyjdONRaaufM7hrg-XguzRpKyTeoTpQmdPE09O-bTC3kyKsDFA1H3zY2xklUzTTSjf2VdoboVPR4bt-507Jv-W0kuxRTQdRMQOQ7sqI5j0dIAuQiL6_S_Jr7M7s5Jw3CMiMIYPazRc_GAzsub0bt-rPVQUkBH1Hg";
         private readonly Uri redirectUrl = new Uri("https://your-app-redirect-url.com");
         private readonly Client client;
 
@@ -52,6 +53,12 @@ namespace ApiUnitTest
         public async void GetOwnProfileMethod()
         {
             var profile = await client.GetOwnProfileAsync();
+            var profileJson = profile.ToJson();
+
+            Assert.Contains("id", profileJson);
+            Assert.Contains("firstName", profileJson);
+            Assert.Contains("lastName", profileJson);
+            Assert.Contains("profilePicture", profileJson);
         }
     }
 }
